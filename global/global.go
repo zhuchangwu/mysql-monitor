b85e480d19ed0e383964300c3f593bdd04141b1e
zhuchangwu_dxm <zhuchangwu@duxiaoman.com>
2020-07-04 23:07:07 +0800

    全局变量

--- global/global.go
+++ global/global.go
@@ -1,5 +1,24 @@
-package common
+package global
 
+import (
+	"fmt"
+	"mysql-monitor/connector"
+)
+
+// 常量
 const (
 	CPUITEM = "CPUITEM"
 )
+
+// 数据库
+var DB *connector.Conenctor
+
+// 初始化数据库连接
+func init() {
+	fmt.Println("Ready to init DB...")
+	DB = &connector.Conenctor{}
+	DB.Open()
+	fmt.Println("Ready to init DB Done")
+}
+
+
