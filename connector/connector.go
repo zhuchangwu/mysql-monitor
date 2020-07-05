378e555fbe1720e9e6d2dbf46db4eacc0659a37f
zhuchangwu_dxm <zhuchangwu@duxiaoman.com>
2020-07-05 12:51:21 +0800

    DB连接、添加统一错误处理，完善Query函数

--- connector/connector.go
+++ connector/connector.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"mysql-monitor/common"
+
 	// mysql驱动
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -31,7 +33,11 @@ type BaseInfo struct {
 /**
  * 查询结果
  */
-type QueryResult struct {
+type QueryResults struct {
+	LastInsertId int64
+	EffectRow    int64
+	Err          error
+	Rows         *sql.Rows
 }
 
 // 读取配置文件
@@ -56,33 +62,54 @@ func (c *Conenctor) Open() {
 	// 读取配置
 	c.loadConfig()
 	dataSource := c.BaseInfo.RootUserName + ":" + c.BaseInfo.RootPassword + "@tcp(" + c.BaseInfo.Addr + ":" + c.BaseInfo.Port + ")/" + c.BaseInfo.DBName
-	db, err := sql.Open("mysql", dataSource)
-	if err != nil {
-		fmt.Printf("Fail to opendb dataSource:[%v] err:[%v]", dataSource, err.Error())
+	db, Err := sql.Open("mysql", dataSource)
+	if Err != nil {
+		common.Error("Fail to opendb dataSource:[%v] Err:[%v]", dataSource, Err.Error())
 		return
 	}
 	db.SetMaxOpenConns(500)
 	db.SetMaxIdleConns(200)
 	c.DB = db
-	err = db.Ping()
-	if err != nil {
-		fmt.Printf("Fail to Ping DB err :[%v]", err.Error())
+	Err = db.Ping()
+	if Err != nil {
+		fmt.Printf("Fail to Ping DB Err :[%v]", Err.Error())
 		return
 	}
 }
 
 // 查询
-func (c *Conenctor) Query(ctx *context.Context, sqlText string, params ...interface{}) (qr *QueryResult, err error) {
-	return &QueryResult{}, nil
+func (c *Conenctor) Query(ctx context.Context, sqlText string, params ...interface{}) (qr *QueryResults) {
+	rows, err := c.DB.QueryContext(ctx, sqlText, params...)
+	qr = &QueryResults{}
+	defer HandleException()
+	if err != nil {
+		qr.Err = err
+		common.Error("Fail to exec qurey sqlText:[%v] params:[%v] err:[%v]", sqlText, params, err)
+		return
+	}
+	qr.Rows = rows
+	return
 }
 
 // 插入、更新、删除
-func (c *Conenctor) Exec(ctx context.Context, sqlText string, params ...interface{}) (affectedRow int, err error) {
-	result, err := c.DB.ExecContext(ctx,sqlText, params...)
-	if err!=nil{
-		affectedRow = 0
+func (c *Conenctor) Exec(ctx context.Context, sqlText string, params ...interface{}) (qr *QueryResults) {
+	qr = &QueryResults{}
+	result, err := c.DB.ExecContext(ctx, sqlText, params...)
+	defer HandleException()
+	if err != nil {
+		qr.EffectRow = 0
+		qr.Err = err
+		common.Error("Fail to exec qurey sqlText:[%v] params:[%v] err:[%v]", sqlText, params, err)
 		return
 	}
-	affected, err := result.RowsAffected()
-	return int(affected), err
+	qr.EffectRow, _ = result.RowsAffected()
+	qr.LastInsertId, _ = result.LastInsertId()
+	return
+}
+
+// 统一的panic处理
+func HandleException() {
+	if err := recover(); err != nil {
+		common.Warn("Connector.go error:[%v]",err)
+	}
 }
