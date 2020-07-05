8c6ae685718fca649f7da53b4926a411f656bcfa
zhuchangwu_dxm <zhuchangwu@duxiaoman.com>
2020-07-05 20:37:28 +0800

    完善CPU监控相关

--- sys/sys.go
+++ sys/sys.go
@@ -2,7 +2,7 @@ package sys
 
 import (
 	"context"
-	"fmt"
+	"mysql-monitor/common"
 	"mysql-monitor/dao"
 	"mysql-monitor/global"
 	"mysql-monitor/util"
@@ -21,6 +21,13 @@ type Referce struct {
 	Cycle     int     //默认单位秒
 	Threshold float64 //默认单位秒
 }
+
+type ChildGoroutineErrInfo struct {
+	ItemName  string // 采集项的名称
+	ErrType   string // 反馈的错误信息类型
+	ErrorInfo string // 具体的错误信息
+}
+
 type SystemMonitor struct {
 	// 上下文
 	context context.Context
@@ -32,21 +39,21 @@ type SystemMonitor struct {
 	referMap map[ItemName]Referce
 
 	// 收集:子goroutine中的错误信息
-	errChan chan ItemName
+	errChan chan *ChildGoroutineErrInfo
 }
 
 // 简单工厂模式
 func GenerateSingletonSystemMonitor() *SystemMonitor {
 	// 读取DB，加载默认的采集周期和报警阈值到内存中
-	m := make(map[ItemName]Referce, 128)
+	m := make(map[ItemName]Referce, 24)
 	m[global.CPUITEM] = Referce{
 		10,
-		0.95,
+		0.00,
 	}
 	return &SystemMonitor{
 		context:  context.Background(),
 		referMap: m,
-		errChan:  make(chan ItemName, 128),
+		errChan:  make(chan *ChildGoroutineErrInfo, 256),
 	}
 }
 
@@ -54,12 +61,13 @@ func GenerateSingletonSystemMonitor() *SystemMonitor {
  * 监控CPU的利用率
  */
 func (s *SystemMonitor) SysCPUUsageRate() {
-	// todo 测试，子goroutinepanic后，父任务可以收到通知
-
+	// 当前goroutine panic后，父任务可以收到通知
+	defer s.handleException(global.CPUITEM,global.PANIC)
 	for {
 		// 获取采集周期和采集时间
 		referce := s.referMap[global.CPUITEM]
 		ticker := time.NewTicker(time.Second * time.Duration(referce.Cycle))
+		common.Info("SysCPUUsageRateMonitor cycle:[%v]", referce.Cycle)
 		// 定时采集
 		select {
 		case <-ticker.C:
@@ -69,12 +77,10 @@ func (s *SystemMonitor) SysCPUUsageRate() {
 			var loadShell = "uptime"
 			loadAvg, status, err := util.SyncExecShell(loadShell)
 			if status == 127 {
-				fmt.Printf("Fail to exec shell:[%v] err:[%v]", loadShell, err.Error())
+				common.Error("Fail to exec shell:[%v] err:[%v]", loadShell, err.Error())
 			}
-
 			// todo 假数据
 			loadAvg = "09:44:36 up 198 days, 21:31,  2 users,  load average: 0.04, 0.01, 0.05"
-
 			// 获取当前采集的时间点: 09:44:36
 			time := util.SubStringWithStartEnd(loadAvg, 0, 7)
 			// 系统运行时常
@@ -90,7 +96,7 @@ func (s *SystemMonitor) SysCPUUsageRate() {
 			for i, v := range arr {
 				float, err := strconv.ParseFloat(v, 64)
 				if err != nil {
-					fmt.Printf("Fail to parse %v to flaot , err:[%v]", v, err.Error())
+					common.Error("Fail to parse %v to flaot , err:[%v]", v, err.Error())
 					return
 				}
 				loadNum[i] = float
@@ -101,11 +107,11 @@ func (s *SystemMonitor) SysCPUUsageRate() {
 			// 假数据
 			cpuNum = "2"
 			if status == 127 {
-				fmt.Printf("Fail to exec shell:[%v] err:[%v]", cpuNumShell, err.Error())
+				common.Error("Fail to exec shell:[%v] err:[%v]", cpuNumShell, err.Error())
 			}
 			num, err := strconv.Atoi(cpuNum)
 			if err != nil {
-				fmt.Printf("Fail to cpuNum:[%v]  to int err:[%v]", cpuNum, err)
+				common.Error("Fail to cpuNum:[%v]  to int err:[%v]", cpuNum, err)
 				return
 			}
 			// 计算平均负载情况：
@@ -117,15 +123,34 @@ func (s *SystemMonitor) SysCPUUsageRate() {
 			avgLoad = avgLoad / 10
 			// 落库
 			cpuInfo := dao.NewCpuInfo(currentTime, time, global.CPUITEM, users, loadNum[0], loadNum[1], loadNum[2], sysRunTime, num, avgLoad)
-			id, err := cpuInfo.InsertOneCord()
-			if err != nil || id == 0 {
-				fmt.Printf("Fail to insert cpuInfo err:[%v]", err.Error())
-				// todo 考虑下，当子goroutine执行出错时，下一步的动作是什么？
+			qr, err := cpuInfo.InsertOneCord()
+			if err != nil {
+				common.Error("Fail to insert cpuInfo err:[%v]", err.Error())
+				 // 向父goroutine汇报
+				 s.handleException(global.CPUITEM,global.INSERT_CPUINFO_ERR)
 			}
+			if qr.LastInsertId == 0 {
+				common.Error("Fail to insert cpuInfo LastInsertId:[%v]", qr.LastInsertId)
+				s.handleException(global.CPUITEM,global.INSERT_CPUINFO_ERR)
+			} else {
+				common.Info("Insert to cpuInfo successful id:[%v] ", qr.LastInsertId)
+			}
+
 			// 如果平均负载大于等于报警项，落库,计数+1
 			if avgLoad >= referce.Threshold {
+				common.Warn("Warning avgLoad:[%v] has been greater than referce.Threshold:[%v]", avgLoad, referce.Threshold)
 				monitor := dao.NewMonitor(global.CPUITEM)
-				monitor.SaveOrUpdate()
+				qr := monitor.SaveOrUpdateCpuInfo()
+				if qr.Err != nil {
+					common.Warn("Fail to update monitor err:[%v]", qr.Err.Error())
+					s.handleException(global.CPUITEM,global.UPDATE_CPUMONITORINFO_ERR)
+				}
+				if qr.EffectRow == 0 {
+					common.Warn("Fail to update monitor EffectRow 0")
+					s.handleException(global.CPUITEM,global.UPDATE_CPUMONITORINFO_ERR)
+				} else {
+					common.Info("Update to monitor successful itemName:[%v] ", global.CPUITEM)
+				}
 			}
 		}
 	}
@@ -143,4 +168,44 @@ func (s *SystemMonitor) SysIOUsageRate() {
  */
 func (s *SystemMonitor) PersistenceItem() {
 
+}
+
+/**
+ *  统一panic处理，当负责采集信息当goroutinpanic后在此函数中重新启动向父goroutine中发送信号
+ */
+func (s *SystemMonitor) handleException(itemName string,errorType string) {
+	if err := recover(); err != nil {
+		common.Warn("gorountine panic will send msg to parent gorutine, msg:[%v]", err)
+		s.errChan <- &ChildGoroutineErrInfo{
+			ItemName:  itemName,
+			ErrType:  errorType,
+			ErrorInfo: err.(error).Error(),
+		}
+	}
+}
+
+/**
+ * 将
+ */
+func (s *SystemMonitor) handlePanicAndAlarm() {
+	for {
+		select {
+		case panicInfo := <-s.errChan:
+			switch panicInfo.ErrType {
+			case global.PANIC:
+				common.Warn("recieve child goroutine panic msg：panicInfo:[%v]", panicInfo)
+				common.Warn("restart goroutine")
+
+			case global.INSERT_CPUINFO_ERR:
+				// todo
+
+			case global.UPDATE_CPUMONITORINFO_ERR:
+				// todo
+
+			}
+		default:
+			common.Info("nothing todo will sleep 2 second")
+			time.Sleep(time.Second * 2)
+		}
+	}
 }
--- sys/sys_test.go
+++ sys/sys_test.go
@@ -8,11 +8,63 @@ import (
 	"time"
 )
 
-func TestMonitorCpu(t *testing.T){
+func TestMonitorChildGroutine(t *testing.T) {
+	type Task struct {
+		ItemName string
+		FuncName string
+		ErrorMsg string
+	}
+	tasks := make(chan *Task, 10)
+
+	handlePanic := func() {
+		if err := recover(); err != nil {
+			fmt.Println("gorountine panic will send msg to parent gorutine")
+			tasks <- &Task{
+				ItemName: "cpuMonitor",
+				FuncName: "cpuMonitorFunc",
+				ErrorMsg: "error~~~",
+			}
+		}
+	}
+
+	go func() {
+		defer handlePanic()
+		// 模拟工作10秒后panic退出了
+		for {
+			fmt.Println("child goroutine start work～～～")
+			time.Sleep(5 * time.Second)
+			panic("wow! panic")
+		}
+
+		fmt.Println("child goroutine panic, send msg to parent")
+		tasks <- &Task{
+			ItemName: "cpuMonitor",
+			FuncName: "cpuMonitorFunc",
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case task:= <- tasks:
+				fmt.Println("有子goroutine退出了～")
+				fmt.Printf("%#v",*task)
+				fmt.Println("报警～")
+			default:
+				fmt.Println("nothingtodo～")
+				time.Sleep(2 * time.Second)
+			}
+
+		}
+	}()
+
+	time.Sleep(60 * time.Second)
+}
+
+func TestMonitorCpu(t *testing.T) {
 	monitor := GenerateSingletonSystemMonitor()
 	monitor.SysCPUUsageRate()
 }
-
 
 /**
  * 定时操作
@@ -29,10 +81,7 @@ func TestTypeParse(t *testing.T) {
 	fmt.Println("done")
 }
 
-/**
- *
- */
-func TestTime(t *testing.T)  {
+func TestTime(t *testing.T) {
 	now := time.Now()
 	fmt.Println(now)
 }
