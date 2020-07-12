048e06a4aa46518bf6f57268a741d09440ec118d
zhuchangwu_dxm <zhuchangwu@duxiaoman.com>
2020-07-12 23:50:57 +0800

    将grpc服务的启动逻辑移动到pb下的rpc_server.go中，解决循环引用，添加rpc响应状态码相关常量

--- global/global.go
+++ global/global.go
@@ -1,16 +1,9 @@
 package global
 
 import (
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/keepalive"
 	"mysql-monitor/common"
 	"mysql-monitor/connector"
-	monitor "mysql-monitor/pb/monitor"
 	task "mysql-monitor/pb/monitor/task"
-	"net"
-	"os"
-	"sync"
-	"time"
 )
 
 // 数据库连接
@@ -30,17 +23,24 @@ const (
 
 // 常量：（操作系统系统相关）采集过程中，子goroutine向父goroutine汇报的错误类型
 const (
-	PANIC                             = "panic"                           // panic
-	SYS_INSERT_CPUINFO_ERR            = "insert-cpu-info-err"             // 保存采集到的cpu信息时报错
-	SYS_INSERT_MEMORY_ERR             = "insert-memory-err"               // 保存采集到的内存使用情况信息时报错
-	SYS_INSERT_TASKS_ERR              = "insert-tasks-err"                // 保存采集到的cpu信息时报错失败
-	SYS_INSERT_STOREINFO_ERR          = "insert-store-info-err"           // 保存采集到的存储的读写IO时报错
-	SYS_INSERT_NETWORKCARDIORATE_ERR  = "insert-network-card-io-rate-err" // 保存采集到的流经网卡的流量失败
-	SYS_INSERT_DISKRANDOMIO_ERR       = "insert-disk-random-io-err"       // 保存采集到的磁盘随机读写失败
-	SYS_UPDATE_CPUMONITORINFO_ERR     = "update-cpu-monitor-info_err"     // 更新CPU—Motior表时报错
-	SYS_UPDATE_CPUUSAGEINFO_ERR       = "update-cpu-usage-info-err"       // 更新CPU使用率报错
-	SYS_UPDATE_MEMORYINFO_ERR         = "update-memory-info-err"          // 更新内存相关监控时报错
-	SYS_UPDATE_ITEM_DISKUSAGERATE_ERR = "update-item-disk-usage-rate-err" // 更新磁盘使用率时报错
+	PANIC                               = "panic"                             // panic
+	SYS_INSERT_CPUINFO_ERR              = "insert-cpu-info-err"               // 保存采集到的cpu信息时报错
+	SYS_INSERT_MEMORY_ERR               = "insert-memory-err"                 // 保存采集到的内存使用情况信息时报错
+	SYS_INSERT_TASKS_ERR                = "insert-tasks-err"                  // 保存采集到的cpu信息时报错失败
+	SYS_INSERT_STOREINFO_ERR            = "insert-store-info-err"             // 保存采集到的存储的读写IO时报错
+	SYS_INSERT_NETWORKCARDIORATE_ERR    = "insert-network-card-io-rate-err"   // 保存采集到的流经网卡的流量失败
+	SYS_INSERT_DISKRANDOMIO_ERR         = "insert-disk-random-io-err"         // 保存采集到的磁盘随机读写失败
+	SYS_UPDATE_CPUMONITORINFO_ERR       = "update-cpu-monitor-info_err"       // 更新CPU—Motior表时报错
+	SYS_UPDATE_DISKUSAGEMONITORINFO_ERR = "update-diskusage-monitor-info_err" // 更新diskusage—Motior表时报错
+	SYS_UPDATE_CPUUSAGEINFO_ERR         = "update-cpu-usage-info-err"         // 更新CPU使用率报错
+	SYS_UPDATE_MEMORYINFO_ERR           = "update-memory-info-err"            // 更新内存相关监控时报错
+	SYS_UPDATE_ITEM_DISKUSAGERATE_ERR   = "update-item-disk-usage-rate-err"   // 更新磁盘使用率时报错
+)
+
+// grpc 状态码
+const (
+	RPC_RES_SUCCESS = iota
+	RPC_RES_FAIL
 )
 
 // 收集:子goroutine中的错误信息
@@ -60,42 +60,5 @@ func init() {
 	AppHotLoadChan = make(chan *task.AppMsg, 256)
 	MysqlHotLoadChan = make(chan *task.MySQLMsg, 256)
 	common.Info("Sys、App、Mysql Hot Load chan make successfully")
-}
-
-
-// 构建Server
-func NewRpcServer() (grpcServer *grpc.Server){
-	var serverOptions []grpc.ServerOption
-	// todo 这里的时间从配置文件中读取
-	serverOptions = append(serverOptions, grpc.KeepaliveParams(keepalive.ServerParameters{
-		MaxConnectionIdle: 10 * time.Second,
-		Time:              10 * time.Second,
-		Timeout:           10 * time.Second,
-	}))
-	common.Info("Prepare to build Grpc-Server")
-	// 构建server
-	grpcServer = grpc.NewServer(serverOptions...)
-	return grpcServer
-}
-
-// 启动Server
-func StartRpcServer(grpcServer *grpc.Server,waitGroup *sync.WaitGroup) {
-	// todo ip、port 从配置文件中读
-	lis, err:= net.Listen("tcp", ":8082")
-	if err != nil {
-		common.Warn("Grpc-Server start fail error:[%v]", err)
-		os.Exit(1)
-		return
-	}
-	// 注册server
-	task.RegisterFlowServiceServer(grpcServer, new(monitor.MySQLMonitorService))
-	err = grpcServer.Serve(lis)
-	if err != nil {
-		common.Warn("Grpc-Server fail to exec grpcServer.Serve(lis) err:[%v]", err)
-		return
-	}
-	waitGroup.Done()
-	common.Info("Grpc Server successful")
-
 }
 
