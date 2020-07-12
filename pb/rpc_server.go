295583b4ef3c67abd0793817c60823c812db4be3
zhuchangwu_dxm <zhuchangwu@duxiaoman.com>
2020-07-12 23:52:06 +0800

    完善消费动态修改系统监控cycle和threshold

--- pb/rpc_server.go
+++ pb/rpc_server.go
@@ -1,17 +1,64 @@
-package monitor
+package pb
 
 import (
 	"context"
 	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 	"mysql-monitor/common"
 	"mysql-monitor/dao"
 	"mysql-monitor/global"
 	task "mysql-monitor/pb/monitor/task"
+	"net"
+	"os"
 	"strconv"
+	"sync"
+	"time"
 )
+
+
 
 type MySQLMonitorService struct {
 	task.FlowServiceServer
+}
+
+
+// grpc 使用的响应状态码
+
+// 构建Server
+func NewRpcServer() (grpcServer *grpc.Server) {
+	var serverOptions []grpc.ServerOption
+	// todo 这里的时间从配置文件中读取
+	serverOptions = append(serverOptions, grpc.KeepaliveParams(keepalive.ServerParameters{
+		MaxConnectionIdle: 10 * time.Second,
+		Time:              10 * time.Second,
+		Timeout:           10 * time.Second,
+	}))
+	common.Info("Prepare to build Grpc-Server")
+	// 构建server
+	grpcServer = grpc.NewServer(serverOptions...)
+	return grpcServer
+}
+
+// 启动Server
+func StartRpcServer(grpcServer *grpc.Server, waitGroup *sync.WaitGroup) {
+	// todo ip、port 从配置文件中读
+	lis, err := net.Listen("tcp", ":8082")
+	if err != nil {
+		common.Warn("Grpc-Server start fail error:[%v]", err)
+		os.Exit(1)
+		return
+	}
+	// 注册server
+	task.RegisterFlowServiceServer(grpcServer, new(MySQLMonitorService))
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		common.Warn("Grpc-Server fail to exec grpcServer.Serve(lis) err:[%v]", err)
+		return
+	}
+	waitGroup.Done()
+	common.Info("Grpc Server successful")
+
 }
 
 // 处理请求
@@ -30,11 +77,11 @@ func (m *MySQLMonitorService) SendTaskToMysqlMonitor(c context.Context, req *tas
 		monitor.Threshold = req.Sysmsg.Threshold
 		qs := monitor.UpdateMonitorCycleOrThresholdInfo()
 		if qs.Err != nil {
-			res.Status = common.RPC_RES_FAIL
+			res.Status = global.RPC_RES_FAIL
 			res.ResponseMsg = "Fail to update sys_monitor table err: " + qs.Err.Error()
 			return res, qs.Err
 		}
-		res.Status = common.RPC_RES_SUCCESS
+		res.Status = global.RPC_RES_SUCCESS
 		res.ResponseMsg = "Update sys_monitor table successful"
 		//将消息写入chan，Sys.go中有协程消费这个chan
 		global.SysHotLoadChan <- req.Sysmsg
