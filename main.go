954231fde5d941d7db27c6aab7e5601f52d2a404
zhuchangwu_dxm <zhuchangwu@duxiaoman.com>
2020-07-12 19:50:21 +0800

    完善mysql-monitor启动时，启动rpc服务器的逻辑

--- main.go
+++ main.go
@@ -1,17 +1,52 @@
 package main
 
-import "runtime"
+import (
+	"mysql-monitor/common"
+	task "mysql-monitor/pb/monitor"
+	"os"
+	"os/signal"
+	"runtime"
+	"sync"
+	"syscall"
+	"time"
+)
 // 初始化DB
 import _ "mysql-monitor/global"
 // 初始化log
 import _ "mysql-monitor/common"
 
+// 考虑重定向标准输出到日志文件。(因为panic级别的错误会在控制台打印)
 func main() {
 	runtime.GOMAXPROCS(1)
-	// 启动gprc服务器，通过	wg := new(sync.WaitGroup) 控制启动顺序
+	// 解析配置文件，将配置文件中的配置解析全局的 配置结构体中
 
-	// todo 将 monitor中各个监控项的监控周期、报警阈值加载进map中。
-	// todo checkMonitorEnv 在启动的过程中，查询检查一下诸如cpu这种监控项是否在monitor表中已存在，如果不存在的话，启动报错。
+	// precheck
+	// checkMonitorEnv 在启动的过程中，查询检查一下诸如cpu这种监控项是否在monitor表中已存在，如果不存在的话，启动报错。
 
-	// 启动工作启动工作
+	// loadData
+	// 将 monitor中各个监控项的监控周期、报警阈值加载进map中。
+
+	// 启动Grpc-Server
+	waitGroup:=new(sync.WaitGroup)
+	waitGroup.Add(1)
+	server:= task.NewRpcServer()
+	go task.StartRpcServer(server,waitGroup)
+
+	// 启动过程中出现异常后优雅的退出
+	sc := make(chan os.Signal)
+	signal.Notify(sc,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+	)
+	for {
+		time.Sleep(1 * time.Second)
+		switch sig := <-sc; sig {
+		case syscall.SIGINT, syscall.SIGTERM:
+			common.Warn("[MysqlMonitor] start failed signal:%v", sig)
+			os.Exit(0)
+		default:
+			continue
+		}
+	}
+
 }
--- pb/monitor/rpc_server.go
+++ pb/monitor/rpc_server.go
@@ -3,18 +3,62 @@ package monitor
 import (
 	"context"
 	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
+	"mysql-monitor/common"
 	task "mysql-monitor/pb/monitor/task"
+	"net"
+	"os"
+	"sync"
+	"time"
 )
 
 type MySQLMonitorService struct {
 	task.FlowServiceServer
 }
 
-func (m *MySQLMonitorService)SendTaskToMysqlMonitor(c context.Context,req *task.RpcSysTask) (*task.Response, error){
+// 构建Server
+func NewRpcServer() (grpcServer *grpc.Server){
+	var serverOptions []grpc.ServerOption
+	// todo 这里的时间从配置文件中读取
+	serverOptions = append(serverOptions, grpc.KeepaliveParams(keepalive.ServerParameters{
+		MaxConnectionIdle: 10 * time.Second,
+		Time:              10 * time.Second,
+		Timeout:           10 * time.Second,
+	}))
+	common.Info("Prepare to build Grpc-Server")
+	// 构建server
+	grpcServer = grpc.NewServer(serverOptions...)
+	return grpcServer
+}
+
+// 启动Server
+func StartRpcServer(grpcServer *grpc.Server,waitGroup *sync.WaitGroup) {
+	// todo ip、port 从配置文件中读
+	lis, err:= net.Listen("tcp", ":8082")
+	if err != nil {
+		common.Warn("Grpc-Server start fail error:[%v]", err)
+		os.Exit(1)
+		return
+	}
+	// 注册server
+	task.RegisterFlowServiceServer(grpcServer, new(MySQLMonitorService))
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		common.Warn("Grpc-Server fail to exec grpcServer.Serve(lis) err:[%v]", err)
+		return
+	}
+	waitGroup.Done()
+	common.Info("Grpc Server successful")
+
+}
+
+// 处理请求
+func (m *MySQLMonitorService) SendTaskToMysqlMonitor(c context.Context, req *task.RpcSysTask) (*task.Response, error) {
 
 	// 解析request
 	// printFlowInfo(request)
-	switch 	req.ItemType {
+	switch req.ItemType {
 	case task.Type_Sys:
 		fmt.Println("接受到了sys消息")
 	case task.Type_App:
